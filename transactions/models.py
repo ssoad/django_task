07da0b4b097ba9c2598e83bd14179543d6dbb6b9
Sohanuzzaman Soad <mdsoad@gmail.com>
2022-07-01 03:19:00 +0600

    First Commit

---
+++ transactions/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from usersinfo.models import Customer
+
+# Create your models here.
+class Transaction(models.Model):
+    sender = models.ForeignKey(Customer, to_field="user", on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(Customer, to_field="user", on_delete=models.CASCADE, related_name="receiver")
+    amount = models.FloatField(null=False, blank=False)
+    datetime = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return str(self.sender.user.first_name+"-"+self.receiver.user.first_name+":"+str(self.amount))
---
+++ usersinfo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+# Create your models here.
+
+class Customer(models.Model):
+    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    balance = models.FloatField(default=0)
+
+
+    def __str__(self):
+        return self.phone
